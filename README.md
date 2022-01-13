d5c2c1ac475e8b41e584e403d977eb80304dcd6a
Julian Lehrer <jmlehrer@ucsc.edu>
2022-01-12 19:32:12 -0800

    Added dataset

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ConvNeXt-lightning
-An implementation of the ConvNeXt architecture built on the PyTorch-Lightning API
+# ConvNeXt-Lightning
+An implementation of the ConvNeXt architecture built on the PyTorch-Lightning API. The base model code (forward passes and architecture) are from the FAIR repo [here](https://github.com/facebookresearch/ConvNeXt).
+
+This library allows easy loading of an untrained ConvNeXt model from the PyTorch Lightning API. Additionally, it provides an `ImageDataset(nn.Dataset)` module for image classification tasks. 
+
+
---
+++ src/data/imagedataset.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import torch
+from torch import Dataset
+import pandas as pd 
+import os 
+
+class ImageSet(Dataset):
+    def __init__(
+        self, 
+        image_path, 
+        label_path, 
+        label_col='labels',
+        name_col='file',
+    ) -> None:
+        super().__init__()
+        self.image_path = image_path
+        self.label_df = pd.read_csv(label_path)
+
+        self.labels = self.label_df.loc[:, label_col]
+        self.filenames = self.label_df.loc[:, name_col]
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, idx):
+        img_name, label = self.filenames.iloc[idx], self.labels.iloc[idx]
+        img = Image.open(os.path.join(self.image_path, img_name))
+        return self.tensor(img), label
